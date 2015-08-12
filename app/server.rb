5259af245e2ed2f3f4b635a266fbf6d99ae983a2
lewis <ljones140@gmail.com>
2015-08-12 15:57:38 +0100

    password confirmation working

--- app/server.rb
+++ app/server.rb
@@ -1,12 +1,15 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require './data_mapper_setup'
+require_relative 'apphelpers.rb'
 
 class Bookmark_Manager < Sinatra::Base
 
-  set :views, proc { File.join(root,'..','views') }
-
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
+
+  set :views, proc { File.join(root,'..','views') }
 
   get '/' do
     erb :index
@@ -38,15 +41,20 @@ class Bookmark_Manager < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(email: params[:email], password: params[:password])
-    session[:user_id] = user.id
-    redirect to('/')
+    user = User.create(email: params[:email],
+                       password: params[:password],
+                       password_confirmation: params[:password_confirmation])
+    if user.save
+      session[:user_id] = user.id
+      redirect to('/links')
+    else
+    flash.now[:notice] = "Password and confirmation password do not match"
+    erb :'users/new'
+    end
   end
 
   helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
+     include AppHelpers
   end
 
 
--- spec/features/user_managment_spec.rb
+++ spec/features/user_managment_spec.rb
@@ -3,17 +3,29 @@ require 'spec_helper'
 feature 'User sign up' do
 
   scenario 'I can sign up as a new user' do
+
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
 
+  scenario 'requires a matching confirmation password' do
+
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Password and confirmation password do not match'
+  end
+
+
+
   def sign_up(email: 'alice@example.com',
-              password: 'oranges!')
+              password: '12345678',
+              password_confirmation: '12345678')
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email, with: email
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
   end
 end
